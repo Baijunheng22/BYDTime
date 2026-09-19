6475a3e94d2e72288bf522096a95ae95d47bfda5
Baijunheng22 <9h429qxdgx@privaterelay.appleid.com>
2026-09-20 00:00:04 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='byd-time-v0.2';
+const CACHE='byd-time-v0.2.1-polish';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
